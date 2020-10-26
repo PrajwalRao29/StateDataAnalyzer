3fd8bdf61563355ad4d7fbe5dff1d38a18a4c8df
PrajwalRao29 <raoprajrao@gmail.com>
2020-10-26 20:32:26 +0530

    Tested code for wrong type file

--- src/main/java/StateCensusAnalyzer.java
+++ src/main/java/StateCensusAnalyzer.java
@@ -8,7 +8,18 @@ import java.util.Iterator;
 
     public class StateCensusAnalyzer {
 
+        public static boolean checkType(String path)
+        {
+            if(path.contains(".csv"))
+                return true;
+            else
+                return false;
+        }
         public int readData(String path) throws CustomException{
+            if(!checkType(path))
+            {
+                throw new CustomException();
+            }
             int count = 0;
             try {
                 Reader reader = Files.newBufferedReader(Paths.get(path));
--- src/test/java/TestStateCensus.java
+++ src/test/java/TestStateCensus.java
@@ -1,17 +1,22 @@
 import org.junit.*;
 public class TestStateCensus {
-    StateCensusAnalyzer c=new StateCensusAnalyzer();
-  
+    StateCensusAnalyzer c = new StateCensusAnalyzer();
+
 
     @Test
-    public void test1_CorrectFile() throws CustomException{
+    public void test1_CorrectFile() throws CustomException {
         int count = c.readData("C:\\Users\\Latha r rao\\Intellij\\StateCensusAnalyzer\\src\\StateCensusData.csv");
-        Assert.assertEquals(29,count);
+        Assert.assertEquals(29, count);
     }
 
     @Test(expected = CustomException.class)
-    public void test2_IncorrectFile() throws CustomException{
-            int count=c.readData("C:\\Users\\Latha r rao\\Intellij\\StateCensusAnalyzer\\StateCensusData.csv");
-        }
+    public void test2_IncorrectFile() throws CustomException {
+        int count = c.readData("C:\\Users\\Latha r rao\\Intellij\\StateCensusAnalyzer\\StateCensusData.csv");
     }
 
+    @Test(expected = CustomException.class)
+    public void test3_CorrectFile_IncorrectType() throws CustomException {
+        int count = c.readData("C:\\Users\\Latha r rao\\Intellij\\StateCensusAnalyzer\\src\\StateCensusData.txt");
+        Assert.assertEquals(29, count);
+    }
+}
